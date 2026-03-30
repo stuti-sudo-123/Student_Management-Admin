e149350186e97da6f5fc2397431ce8c0adfcdb86
stuti-sudo-123 <stutigupta2007@gmail.com>
2026-03-30 12:27:31 +0530

    .

--- adminFuction/AdminMenue.java
+++ adminFuction/AdminMenue.java
@@ -4,5 +4,6 @@ public class AdminMenue {
       public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("-----Welcome Admin----");
+        sc.close();
 }
 }
