6d8177eeb9dacfaff978d01d77459a9309729562
pranathimaddipatla651-sudo <pranathimaddipatla651@gmail.com>
2026-04-10 11:21:40 +0530

    commit

--- connection/DBConnection.java
+++ connection/DBConnection.java
@@ -1,4 +1,3 @@
-package connection;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -7,18 +6,12 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
-<<<<<<< HEAD
 public class DBConnection 
 {
-=======
-public class DBConnection {
-
->>>>>>> 316cd0e69d60269cb48bc1e6b6725ab4648503f5
     private static String URL;
     private static String USER;
     private static String PASSWORD;
 
-<<<<<<< HEAD
     // Load properties when class is first used
     static 
     {
@@ -35,37 +28,6 @@ public class DBConnection {
         catch (IOException e) 
         {
             System.out.println("config.properties file not found!");
-=======
-    // ✅ Everything in ONE static block
-    static {
-        // Load the MySQL driver
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException ex) {
-            System.out.println("MySQL Driver not found: " + ex.getMessage());
-        }
-
-        // Load credentials from config.properties (try classpath + current working dir)
-        Properties props = new Properties();
-        try {
-            java.io.InputStream in = DBConnection.class.getResourceAsStream("/connection/config.properties");
-            if (in == null) {
-                in = new FileInputStream("config.properties");
-            }
-            if (in == null) {
-                throw new IOException("config.properties not found in classpath or working directory");
-            }
-            props.load(in);
-            in.close();
-
-            URL = props.getProperty("db.url");
-            USER = props.getProperty("db.user");
-            PASSWORD = props.getProperty("db.password");
-
-            
-        } catch (IOException e) {
-            System.out.println("config.properties file not found or not readable: " + e.getMessage());
->>>>>>> 316cd0e69d60269cb48bc1e6b6725ab4648503f5
         }
     }
 
